45c5c20ddddb99238b7cc07442f8aa32cfd6838c
BoilTask <BoilTask@qq.com>
2025-04-29 14:34:47 +0800

    完善接口

---
+++ foundation/foundation-service/judge.go
@@ -0,0 +1,29 @@
+package foundationservice
+
+import (
+	"context"
+	"foundation/foundation-dao"
+	foundationmodel "foundation/foundation-model"
+	"meta/singleton"
+)
+
+type JudgeService struct {
+}
+
+var singletonJudgeService = singleton.Singleton[JudgeService]{}
+
+func GetJudgeService() *JudgeService {
+	return singletonJudgeService.GetInstance(
+		func() *JudgeService {
+			return &JudgeService{}
+		},
+	)
+}
+
+func (s *JudgeService) GetJudge(ctx context.Context, id string) (*foundationmodel.JudgeJob, error) {
+	return foundationdao.GetJudgeJobDao().GetJudgeJob(ctx, id)
+}
+
+func (s *JudgeService) GetJudgeList(ctx context.Context, page int, pageSize int) ([]*foundationmodel.JudgeJob, int, error) {
+	return foundationdao.GetJudgeJobDao().GetJudgeJobList(ctx, page, pageSize)
+}
