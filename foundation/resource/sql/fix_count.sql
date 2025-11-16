a9ab81f93dea53ff2c4e1676fe041a94448fb05f
BoilTask <BoilTask@qq.com>
2025-11-16 22:54:25 +0800

    更新脚本

--- foundation/resource/sql/fix_count.sql
+++ foundation/resource/sql/fix_count.sql
@@ -1,24 +1,27 @@
 UPDATE problem p
-JOIN (
+SET
+    attempt = j.attempt,
+    accept  = j.accept
+FROM (
     SELECT
         problem_id,
         COUNT(*) AS attempt,
         SUM(CASE WHEN status = 6 THEN 1 ELSE 0 END) AS accept
     FROM judge_job
     GROUP BY problem_id
-) AS j ON p.id = j.problem_id
-SET p.attempt = j.attempt,
-    p.accept = j.accept;
+) AS j
+WHERE p.id = j.problem_id;
 
-UPDATE user u
-JOIN (
+UPDATE "user" u
+SET
+    attempt = j.attempt,
+    accept  = j.accept
+FROM (
     SELECT
         inserter AS user_id,
         COUNT(*) AS attempt,
         SUM(CASE WHEN status = 6 THEN 1 ELSE 0 END) AS accept
     FROM judge_job
     GROUP BY inserter
-) AS j ON u.id = j.user_id
-SET
-    u.attempt = j.attempt,
-    u.accept = j.accept;
+) AS j
+WHERE u.id = j.user_id;
